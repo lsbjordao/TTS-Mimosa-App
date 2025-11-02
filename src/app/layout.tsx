c3f43d7593ca22fb2bca540caaafcf318e4eb0b1
lsbjordao <tucarj@gmail.com>
2025-11-02 13:11:40 -0300

    fav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "TTS-Mimosa",
   icons: {
-    icon: "/TTS-Mimosa-App/favicon.ico", // ou /favicon.ico, conforme o arquivo que você tiver
+    icon: "/TTS-Mimosa-App/favicon.png",
   }
 };
 
