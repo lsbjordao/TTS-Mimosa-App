7ebd92886dd91270e58f22e2573f5f206bf1de4b
lsbjordao <tucarj@gmail.com>
2025-12-11 19:24:28 -0300

    graphql

--- src/app/graphql/page.tsx
+++ src/app/graphql/page.tsx
@@ -2,15 +2,29 @@
 
 "use client";
 
+import { useEffect, useState } from "react";
 import Header from "./header";
 
 export default function Graphql() {
+  const [html, setHtml] = useState<string>("Carregando...");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8001/graphql")
+      .then((res) => res.text())
+      .then((text) => setHtml(text))
+      .catch((err) => setHtml("Erro ao carregar: " + err));
+  }, []);
 
   return (
     <div className="w-full h-screen flex flex-col bg-background text-foreground overflow-hidden">
       <div className="sticky top-0 z-10 bg-background border-b border-border">
         <Header />
       </div>
+
+      <div
+        className="flex-1 overflow-auto"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
     </div>
   );
 }
